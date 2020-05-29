5fc5aa7a666b5287b0446cf5354732f8669a3523
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-29 01:37:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -7,6 +7,6 @@ def oxford_comma(array)
   elsif array.length == 2
     array[0] + " and " + array[1]
   else array.length > 2
-    array[0] + " , " + array[1] + " , and " array.[2]
+    array[0] + " , " + array[1] + " , and " array[2]
   end
 end
