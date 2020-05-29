c589c2c8b8f9418e041f722315fd08133581d8a0
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-29 01:36:23 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -6,5 +6,7 @@ def oxford_comma(array)
     array[0]
   elsif array.length == 2
     array[0] + " and " + array[1]
+  else array.length > 2
+    array[0] + " , " + array[1] + " , and " array.last
   end
 end
