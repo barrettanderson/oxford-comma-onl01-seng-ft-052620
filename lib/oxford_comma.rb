3fcd4895a0b688a6522842b7adc57bbc4537074c
Barrett Anderson <barrettanderson1989@gmail.com>
2020-05-29 01:37:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,12 +1,11 @@
 require 'pry'
 
 def oxford_comma(array)
-  #.binding.pry
   if array.length == 1
     array[0]
   elsif array.length == 2
     array[0] + " and " + array[1]
-  else array.length > 2
-    array[0] + " , " + array[1] + " , and " array[2]
+  else 
+    binding.pry
   end
 end
